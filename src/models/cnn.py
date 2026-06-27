fe7e3fa4313e62eef2bebc52b860f52e29ac007c
AvantikaSahu2203 <avantikasahu22035@gmmail.com>
2026-06-27 19:08:15 +0530

    Completed first CNN architecture

--- src/models/cnn.py
+++ src/models/cnn.py
@@ -7,16 +7,39 @@ class CropDiseaseCNN(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(
-            in_channels=3,
-            out_channels=32,
-            kernel_size=3,
-            padding=1
+        # Feature Extractor
+        self.features = nn.Sequential(
+
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
         )
 
-        self.relu = nn.ReLU()
+        # Classifier
+        self.classifier = nn.Sequential(
 
-        self.pool = nn.MaxPool2d(
-            kernel_size=2,
-            stride=2
+            nn.Flatten(),
+
+            nn.Linear(128 * 28 * 28, 512),
+            nn.ReLU(),
+
+            nn.Dropout(0.5),
+
+            nn.Linear(512, 38)
         )
+
+    def forward(self, x):
+
+        x = self.features(x)
+
+        x = self.classifier(x)
+
+        return x
